49ecb6c81a21a02ff47644187fbd27d9f6ef9d31
David <david.nvalderrama@gmail.com>
2018-12-16 14:33:27 -0500

    Add article model

--- top_news/models.py
+++ top_news/models.py
@@ -1,20 +1,16 @@
 from django.db import models
 
-class Publication(models.Model):
-    title = models.CharField(max_length=30)
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField
+    url = models.CharField(max_length=255)
+    url_to_image = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateField
+    content = models.TextField
 
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ('title',)
-
-class Article(models.Model):
-    headline = models.CharField(max_length=100)
-    publications = models.ManyToManyField(Publication)
-
-    def __str__(self):
-        return self.headline
-
-    class Meta:
-        ordering = ('headline',)
+        ordering = ('published_at',)
