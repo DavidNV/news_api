601cb643daf26551ecd90e33ad6bc70da1ae78fb
David <david.nvalderrama@gmail.com>
2018-12-17 03:46:19 -0500

    update collector and docker related files

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 ARG inbound_api_key
 FROM python:3
+MAINTAINER david.nvalderrama@gmail.com
 
 ARG inbound_api_key
 ENV API_KEY=$inbound_api_key
@@ -10,6 +11,13 @@ ENV PYTHONUNBUFFERED 1
 RUN mkdir /news_api
 WORKDIR /news_api
 ADD requirements.txt /news_api/
-RUN pip install -r requirements.txt
+
+ADD ./start-exec.sh /news_api/
+#RUN chmod -R 777 ./start-exec.sh
+RUN ls -l /news_api
+
+#ENTRYPOINT ["./start-exec.sh"]
 
 ADD . /news_api
+RUN pip install -r requirements.txt
+#sudo docker-compose build --build-arg inbound_api_key=af92652e76b745a6bde8dd2fc5739bfd
--- top_news/lib/collector.py
+++ top_news/lib/collector.py
@@ -4,15 +4,26 @@ import json
 
 class Collector:
 
+    """
+    News Collector. It's main function its to perform the main request to the news api and return the json value from it.
+    """
+
     def __init__(self):
        self.api_key = self.__get_api_key__()
        self.url = 'https://newsapi.org/v2/top-headlines'
 
-    def retrieve(self, country="us"):
+    def retrieve(self, country):
         params = {"country": country, "apiKey": self.api_key, "pageSize": 100}
-        response = requests.get(self.url, params=params)
-        json_response = response.json()
-        return json_response
+        try:
+            response = requests.get(self.url, params=params)
+            json_response = response.json()
+            return json_response
+        except requests.exceptions.RequestException as e:
+            #TODO Hey, future David, please don't forget to do something with the error. Best, present David.
+            return self.__default_response__()
 
     def __get_api_key__(self):
         return os.environ['API_KEY']
+
+    def __default_response__(self)
+        return {"status": 400, "total_results": 0, "articles": []}
