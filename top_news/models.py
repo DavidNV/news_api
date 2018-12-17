977bce5198a2285a469eb92631bca9fa20475002
David <david.nvalderrama@gmail.com>
2018-12-17 09:57:41 -0500

    Update models

--- top_news/models.py
+++ top_news/models.py
@@ -1,26 +1,25 @@
 from django.db import models
 
-class Article(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField
-    url = models.CharField(max_length=255)
-    url_to_image = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    published_at = models.DateField
-    content = models.TextField
-
-    def __str__(self):
-        return self.title
-
 class Author(models.Model):
     name = models.CharField(max_length=255)
-    articles = models.ManyToManyField(Article)
 
     def __str__(self):
         return self.name
 
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    url = models.CharField(max_length=255)
+    url_to_image = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateField()
+    content = models.TextField(null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
 class Source(models.Model):
-    uid = models.CharField(max_length=70)
+    uid = models.CharField(max_length=70, null=True)
     name = models.CharField(max_length=200)
     articles = models.ManyToManyField(Article)
-    authors = models.ManyToManyField(Author)
